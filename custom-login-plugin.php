6c708e717a4a3aca19a0206b01cd299b0c7a1f17
hermanzuiakov <herman.zuiakov@gmail.com>
2023-07-04 19:05:10 +0200

    Initial commit

---
+++ custom-login-plugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Custom Login Plugin
+Description: Custom Login Plugin with redirect settings, failed attempts counter and AJAX mode for login form.
+Author: Herman Zuiakov
+Author URI: https://github.com/hermanzuiakov
+Version: 1.0
+*/
